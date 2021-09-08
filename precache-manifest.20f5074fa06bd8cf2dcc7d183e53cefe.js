356dd96702a51c96874b725d3cab677d686c7072
soninamrata <soninamrata35@gmail.com>
2021-09-08 21:53:49 +0530

    Updates

---
+++ precache-manifest.20f5074fa06bd8cf2dcc7d183e53cefe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d81169667bb8379c98d60f7c9d0cd19",
+    "url": "/appreact/index.html"
+  },
+  {
+    "revision": "9f8216d4860f326ec747",
+    "url": "/appreact/static/css/main.96ad36ba.chunk.css"
+  },
+  {
+    "revision": "b81443c5dc411840ef45",
+    "url": "/appreact/static/js/2.7a12d061.chunk.js"
+  },
+  {
+    "revision": "9f8216d4860f326ec747",
+    "url": "/appreact/static/js/main.01e94b27.chunk.js"
+  },
+  {
+    "revision": "46c9aefe9a753a431924",
+    "url": "/appreact/static/js/runtime~main.ac1b2f02.js"
+  }
+]);
